f4bf8231798545a52da58d06592f3dab01663c9f
Dmitry Rybakov <dmitry.rybakov@mongodb.com>
2021-11-17 14:44:36 +0100

    Add _bson_to_i to TimeWithZone

--- lib/mongoid/matcher/eq_impl.rb
+++ lib/mongoid/matcher/eq_impl.rb
@@ -27,6 +27,23 @@ module Mongoid
           value.is_a?(Array) && value.include?(condition)
         end
       end
+
+      # Per https://docs.mongodb.com/ruby-driver/current/tutorials/bson-v4/#time-instances,
+      # > Times in BSON (and MongoDB) can only have millisecond precision. When Ruby Time instances
+      # are serialized to BSON or Extended JSON, the times are floored to the nearest millisecond.
+      #
+      # > Because of this flooring, applications are strongly recommended to perform all time
+      # calculations using integer math, as inexactness of floating point calculations may produce
+      # unexpected results.
+      #
+      # As such, perform a similar operation to what the bson-ruby gem does
+      module_function def time_eq?(time_a, time_b)
+        time_rounded_to_millis(time_a) == time_rounded_to_millis(time_b)
+      end
+
+      module_function def time_rounded_to_millis(time)
+        return time._bson_to_i * 1000 + time.usec.divmod(1000).first
+      end
     end
   end
 end
